098b8cb8b32190350720c2c2d83ada737942fa48
Svea Wade <svealinneawade@gmail.com>
2020-10-22 15:20:08 -0700

    added rest of the functions

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,25 @@ function divide(number1, number2) {
 //User interface logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-  event.preventDefault();
-  const number1 = parseInt($("#add1").val());
-  const number2 = parseInt($("#add2").val());
-  const result = add(number1, number2);
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+  const number1 = parseInt($("#input1").val());
+  const number2 = parseInt($("#input2").val());
+  const operator = $("input:radio[name=operator]:checked").val();
+  let result;
+  if (operator === "add") {
+    result = add(number1, number2);
+  } else if (operator === "subtract") {
+    result = subtract(number1, number2);
+  } else if (operator === "multiply") {
+    result = multiply(number1, number2);
+  } else if (operator === "divide") {
+    result = divide(number1, number2);  
+  }
   $("#output").text(result);
   });
 });
+
+  //console.log("1st number: " + number1);  // for debugging
+  //console.log("2nd number: " + number2);  // for debugging
+  //console.log("operator: " + operator);  // for debugging
